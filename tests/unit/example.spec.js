deda86fd630f8e8ffd56b2cab27a2c6c58298d7b
NaikyDing <Naiky.ding@goldenf.co>
2021-02-17 14:44:39 +0800

    [Revise] TEST FAIL

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -5,8 +5,8 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { msg },
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toMatch(sg)
   })
 })
